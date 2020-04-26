09f3f0186dffed2a80f9dc7bba1058614d57f751
guptabless <61534368+guptabless@users.noreply.github.com>
2020-04-26 00:39:23 -0400

    Update SubDomainDumper.py
    Port Scanning functionality added

--- SubDomainDumper.py
+++ SubDomainDumper.py
@@ -1,10 +1,12 @@
 from googlesearch import search
+import socket
+import bcolors
 
 print("------------------------------------------------")
 print("Code BY ::  NG")
 print("------------------------------------------------")
-url = input("Enter URL which Subdomain we want to find by using GOOGLE dork Functionality ")
-print(url)
+
+url = input(bcolors.BLUE + "Enter URL which Subdomain we want to find by using GOOGLE dork Functionality ")
 
 if(url.startswith("https://")):
   new_url = url.replace("https://", "*.")
@@ -13,15 +15,29 @@ else:
 
 surl = new_url
 surl= "site:" + new_url
+host_ip = socket.gethostbyname(new_url)
 
 try:
     from googlesearch import search
 except ImportError:
     print("Not found")
 
-limit = int(input("Please enter the limit how much SubDomain you want to fetch"))
+limit = int(input(bcolors.HEADER +  "Please enter the limit how many SubDomain you want to fetch"))
+
+portScanLowerLimit = int(input(bcolors.HEADER + "Please enter the starting value of port scan"))
+portScanUpperLimit = int(input(bcolors.HEADER + "Please enter the end value of scan"))
 
 for u in search(surl, tld="com", num=10, stop=limit, pause=2):
-  print(u)
+    if (u.startswith("https://")):
+        u_sub_port = u.replace("https://", "")
+    else:
+        u_sub_port = u.replace("http://", "")
+    for port in range(portScanLowerLimit,portScanUpperLimit):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connect = sock.connect_ex((host_ip, port))
+        if connect == 0:
+            print("Port {}:    is Open".format(port))
+            print(bcolors.OKMSG + u_sub_port)
+            sock.close()
 
 
